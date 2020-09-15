6f19cc72c809d8f960e3ea847e693a8f8e5ead33
Mauro <mauro.spinosi94@gmail.com>
2020-09-15 17:03:43 +0200

    Esercizio completo

---
+++ js/menu.js
@@ -0,0 +1,44 @@
+// Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età.Stampare a schermo attraverso il for in tutte le proprietà.
+var studente = {
+  'nome' : 'Mauro',
+  'cognome' : 'Spinosi',
+  'eta' : 26 + " " + "anni",
+};
+
+for(var key in studente){
+  console.log(key + ":" + " " + studente[key]);
+}
+
+// Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+var studenti = [
+  {
+    'nome' : 'Marco',
+    'cognome' : 'Laprea',
+    'eta' : 87,
+  },
+  {
+    'nome' : 'Giuseppe',
+    'cognome' : 'Esposito',
+    'eta' : 32,
+  },
+  {
+    'nome' : 'Carolina',
+    'cognome' : 'Sicignano',
+    'eta' : 21,
+  },
+];
+
+
+// Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+var nuovoOggetto = {
+'nome' : prompt("Inserisci il Nome"),
+'cognome' : prompt("Inserisci il Cognome"),
+'eta' : parseInt(prompt("Inserisci l' età")),
+}
+studenti.push(nuovoOggetto);
+
+for (var i = 0; i < studenti.length; i++){
+  for(var j in studenti[i]){
+    console.log(j + ":" + " " + studenti[i][j]);
+  }
+}
