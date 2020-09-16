777e53c2ceb821a480922e288fc1c27bd47a1b93
Mauro <mauro.spinosi94@gmail.com>
2020-09-16 10:45:15 +0200

    esercizio completo

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,4 @@
 $(document).ready(function () {
-  var source = $("#entry-template").html();
-  var template = Handlebars.compile(source);
-
   // Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età.Stampare a schermo attraverso il for in tutte le proprietà.
   var studente = {
     'nome' : 'Mauro',
@@ -13,40 +10,51 @@ $(document).ready(function () {
     console.log(key + ":" + " " + studente[key]);
   }
 
-  var html = template(studente);
-  $(".student").append(html);
-});
+  // Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+  var studenti = [
+    {
+      'nome' : 'Marco',
+      'cognome' : 'Laprea',
+      'eta' : 87,
+    },
+    {
+      'nome' : 'Giuseppe',
+      'cognome' : 'Esposito',
+      'eta' : 32,
+    },
+    {
+      'nome' : 'Carolina',
+      'cognome' : 'Sicignano',
+      'eta' : 21,
+    },
+  ];
+  var source = $("#entry-template").html();
+  var template = Handlebars.compile(source);
 
-// Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
-var studenti = [
-  {
-    'nome' : 'Marco',
-    'cognome' : 'Laprea',
-    'eta' : 87,
-  },
-  {
-    'nome' : 'Giuseppe',
-    'cognome' : 'Esposito',
-    'eta' : 32,
-  },
-  {
-    'nome' : 'Carolina',
-    'cognome' : 'Sicignano',
-    'eta' : 21,
-  },
-];
+  for (var i = 0; i < studenti.length; i++){
+    console.log(studenti[i].nome + " " + studenti[i].cognome);
 
-
-// Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
-var nuovoOggetto = {
-'nome' : prompt("Inserisci il Nome"),
-'cognome' : prompt("Inserisci il Cognome"),
-'eta' : parseInt(prompt("Inserisci l' età")),
-}
-studenti.push(nuovoOggetto);
-
-for (var i = 0; i < studenti.length; i++){
-  for(var j in studenti[i]){
-    console.log(j + ":" + " " + studenti[i][j]);
+    var context = {
+      'nome' : studenti[i].nome,
+      'cognome' : studenti[i].cognome,
+      'eta' : studenti[i].eta,
+    }
+    var html = template(context);
+    $(".student").append(html);
   }
-}
+
+
+  // Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+  var nuovoOggetto = {
+  'nome' : prompt("Inserisci il Nome"),
+  'cognome' : prompt("Inserisci il Cognome"),
+  'eta' : parseInt(prompt("Inserisci l' età")),
+  }
+  studenti.push(nuovoOggetto);
+
+  for (var i = 0; i < studenti.length; i++){
+    for(var j in studenti[i]){
+      console.log(j + ":" + " " + studenti[i][j]);
+    }
+  }
+});
